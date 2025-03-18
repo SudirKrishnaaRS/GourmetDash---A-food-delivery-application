4c850bdf6d0ece02d099b8468bc5781c7bd300a7
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-03-18 18:38:30 +0530

    Added Restaurant structure

---
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./RestaurantCard.css";
+
+const RestaurantCard = () => {
+  return <div className="card-container">RestaurantCard</div>;
+};
+
+export default RestaurantCard;
